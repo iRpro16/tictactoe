1d1e47a4c4e42dfea2ac888b612861ba2fd96f2b
Isidoro Rumeo <rumeoisidoro16@gmail.com>
2023-03-10 17:38:25 -0500

    Design and some JS

---
+++ script.js
@@ -0,0 +1,14 @@
+const gameboard = (() => {
+   const boardArr = ['', '', '', '', '', '', '', '', '']; 
+   return {
+    boardArr
+   };
+})();
+
+const displayBoard = (() => {
+
+})();
+
+const playerFactory = (name, marker) => {
+    return {name, marker}
+};
