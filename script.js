97e804b86405040a5512046371e4edf88b1e705f
Isidoro Rumeo <rumeoisidoro16@gmail.com>
2023-03-11 18:10:19 -0500

    Figured index issue!!!!

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-const gameboard = (() => {
-   const boardArr = ['', '', '', '', '', '', '', '', '']; 
-   return {
-    boardArr
-   };
+const player = (name, marker) => {
+
+    return {name, marker};
+}
+
+const playerOne = player('ryan', 'x');
+const playerTwo = player('andrew', 'o')
+
+const gameBoard = (() => {
+
+    const board = ['', '', '', '', '', '', '', '', ''];
+    const items = document.querySelectorAll('.item');
+    const itemArr = [...items]
+
+    const displayBoard = () => {
+
+        itemArr.forEach(item => {
+            item.addEventListener('click', () => {
+                let index = itemArr.indexOf(item)
+                item.innerHTML = playerOne.marker
+                board.splice(index, 1, playerOne.marker)
+                console.log(index);
+                console.log(board)
+            })
+        });
+
+    }
+
+    return {
+        displayBoard,
+        board
+    }
 })();
-
-const displayBoard = (() => {
-
-})();
-
-const playerFactory = (name, marker) => {
-    return {name, marker}
-};
+gameBoard.displayBoard();
