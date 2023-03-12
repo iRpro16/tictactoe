7e78a63a6ac1e5809719f3e2825098f08a2b33e7
Isidoro Rumeo <rumeoisidoro16@gmail.com>
2023-03-12 19:25:41 -0400

    Logic to change marker on click

--- script.js
+++ script.js
@@ -3,32 +3,52 @@ const player = (name, marker) => {
     return {name, marker};
 }
 
-const playerOne = player('ryan', 'x');
-const playerTwo = player('andrew', 'o')
-
+// gameBoard module:
 const gameBoard = (() => {
 
     const board = ['', '', '', '', '', '', '', '', ''];
     const items = document.querySelectorAll('.item');
-    const itemArr = [...items]
+    const itemArr = [...items];
+    let playerOne = player('jake', 'x');
+    let playerTwo = player('andrew', 'o');
+    let currentPlayer = playerOne.marker;
+
+
+    const changeTurn = () => {
+        if (currentPlayer == playerOne.marker) {
+            currentPlayer = playerTwo.marker;
+        } else {
+            currentPlayer = playerOne.marker
+        }
+    }
 
     const displayBoard = () => {
 
         itemArr.forEach(item => {
+
             item.addEventListener('click', () => {
-                let index = itemArr.indexOf(item)
-                item.innerHTML = playerOne.marker
-                board.splice(index, 1, playerOne.marker)
-                console.log(index);
-                console.log(board)
+                let index = itemArr.indexOf(item);
+                item.innerHTML = currentPlayer;
+                board.splice(index, 1, currentPlayer);
+                changeTurn();
             })
         });
-
-    }
+    };
 
     return {
         displayBoard,
-        board
-    }
+        board,
+        playerOne,
+        playerTwo,
+        currentPlayer,
+    };
 })();
+
 gameBoard.displayBoard();
+
+// displayController Module:
+const displayController = (() => {
+
+
+})();
+
