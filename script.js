c9448073562f204d0fd6783840d025645975fccc
Isidoro Rumeo <rumeoisidoro16@gmail.com>
2023-03-13 20:52:12 -0400

    Got the winning conditions to work.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const player = (name, marker) => {
 let playerOne = player('tristan', 'x');
 let playerTwo = player('andrew', 'o');
 
+const winDeclare = document.querySelector('.winner-declare');
+
 // gameBoard module:
 const gameBoard = (() => {
 
@@ -23,11 +25,11 @@ const gameBoard = (() => {
                 item.innerHTML = currentPlayer;
                 board.splice(index, 1, currentPlayer);
                 changeTurn();
+                displayController.winConditions()
+
             }, { once: true});
         });
     };
-
-
 
     const changeTurn = () => {
         if (currentPlayer == playerOne.marker) {
@@ -36,6 +38,7 @@ const gameBoard = (() => {
             currentPlayer = playerOne.marker
         }
     };
+
 
     return {
         displayBoard,
@@ -50,5 +53,56 @@ const displayController = (() => {
 
     gameBoard.displayBoard();
 
+    let array = gameBoard.board;
+
+    const winConditions = () => {
+        // Rows 
+        if (array[0] == 'x' && array[1] == 'x' && array[2] == 'x') {
+            winDeclare.innerHTML = `${playerOne.name} wins this round`
+            } else if (array[0] == 'o' && array[1] == 'o' && array[2] == 'o') {
+                winDeclare.innerHTML = `${playerTwo.name} wins this round`
+            } else if (array[3] == 'x' && array[4] == 'x' && array[5] == 'x') {
+                winDeclare.innerHTML = `${playerOne.name} wins this round`
+            } else if (array[3] == 'o' && array[4] == 'o' && array[5] == 'o') {
+                winDeclare.innerHTML = `${playerTwo.name} wins this round`;
+            } else if (array[6] == 'x' && array[7] == 'x' && array[8] == 'x') {
+                winDeclare.innerHTML = `${playerOne.name} wins this round`
+            } else if (array[6] == 'o' && array[7] == 'o' && array[8] == 'o') {
+                winDeclare.innerHTML = `${playerTwo.name} wins this round`;
+            }
+
+        // Columns
+        if (array[0] == 'x' && array[3] == 'x' && array[6] == 'x') {
+            winDeclare.innerHTML = `${playerOne.name} wins this round`
+        } else if (array[0] == 'o' && array[3] == 'o' && array[6] == 'o') {
+            winDeclare.innerHTML = `${playerTwo.name} wins this round`;
+        } else if (array[1] == 'x' && array[4] == 'x' && array[7] == 'x') {
+            winDeclare.innerHTML = `${playerOne.name} wins this round`
+        } else if (array[1] == 'o' && array[4] == 'o' && array[7] == 'o') {
+            winDeclare.innerHTML = `${playerTwo.name} wins this round`;
+        } else if (array[2] == 'x' && array[5] == 'x' && array[8] == 'x') {
+            winDeclare.innerHTML = `${playerOne.name} wins this round`
+        } else if (array[2] == 'o' && array[5] == 'o' && array[8] == 'o') {
+            winDeclare.innerHTML = `${playerTwo.name} wins this round`;
+
+        }
+
+        // Diagonals 
+        if (array[0] == 'x' && array[4] == 'x' && array[8] == 'x') {
+            winDeclare.innerHTML = `${playerOne.name} wins this round`
+        } else if (array[0] == 'o' && array[4] == 'o' && array[8] == 'o') {
+            winDeclare.innerHTML = `${playerTwo.name} wins this round`;
+        } else if (array[2] == 'x' && array[4] == 'x' && array[6] == 'x') {
+            winDeclare.innerHTML = `${playerOne.name} wins this round`
+        } else if (array[2] == 'o' && array[4] == 'o' && array[6] == 'o') {
+            winDeclare.innerHTML = `${playerTwo.name} wins this round`;
+
+        }
+    }
+
+
+    return {
+        winConditions
+    }
 
 })();
